39106364c703ff5f4429d56e924aa108900067d3
Jin Yoon <jinyoon0124@gmail.com>
2016-04-30 17:58:47 -0400

    Ver2. Base Starter Code downloaded TestProvider TestUriMatcher TestWeatherContract TestFetchWeatherTask WeatherProvider
    Edited
    WeatherContract
    FetchWeatherTask

--- app/src/main/java/com/example/jinyoon/a01sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/jinyoon/a01sunshine/ForecastFragment.java
@@ -2,12 +2,9 @@ package com.example.jinyoon.a01sunshine;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,22 +15,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -92,7 +75,7 @@ public class ForecastFragment extends Fragment {
 
 
     public void updateWeather(){
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask(this.getContext(),mforecastAdapter);
         SharedPreferences spr = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String location = spr.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
         //Toast.makeText(this.getContext(), units, Toast.LENGTH_SHORT).show();
@@ -141,182 +124,6 @@ public class ForecastFragment extends Fragment {
 
 
         return rootView;
-    }
-
-    public class FetchWeatherTask extends AsyncTask<String,Void,String[]>{
-
-        //gets class name so that it can be seen on error log
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-        private String getReadableDateString(Date time){
-            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE, MMM dd");
-            return shortenedDateFormat.format(time);
-        }
-
-
-        private String formatHighLows(double high, double low, String units){
-
-            if(units.equals(getString(R.string.pref_units_imperial))){
-                high = high*1.8+32;
-                low = low*1.8+32;
-            }
-
-            long roundedHigh=Math.round(high);
-            long roundedLow = Math.round(low);
-
-            String highLowStr = roundedHigh+"/"+roundedLow;
-            return highLowStr;
-        }
-
-        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays) throws JSONException {
-
-            final String OWM_LIST="list";
-            final String OWM_WEATHER="weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX="max";
-            final String OWN_MIN="min";
-            final String OWM_DESCRIPTION="main";
-
-            JSONObject forecastJson= new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
-
-            Calendar calendar = new GregorianCalendar();
-
-            String[] resultStrs = new String[numDays];
-            for(int i = 0; i<weatherArray.length(); i++){
-                String day;
-                String description;
-                String highAndLow;
-
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-
-                Date result_1 = calendar.getTime();
-                day= getReadableDateString(result_1);
-                calendar.add(GregorianCalendar.DATE,1);
-                //Log.v("!!!DAY TEST: ",day );
-
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                //Log.v("!!! WEATHER OBJECT : ", weatherObject.toString());
-                description = weatherObject.getString(OWM_DESCRIPTION);
-                //Log.v("!!!Description Test:",description);
-
-
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble(OWM_MAX);
-                double low = temperatureObject.getDouble(OWN_MIN);
-
-                SharedPreferences spr = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                String units = spr.getString(getString(R.string.pref_units_key),getString(R.string.pref_units_metric));
-
-                highAndLow=formatHighLows(high, low, units);
-                resultStrs[i]=day+" - "+description+" - "+highAndLow;
-
-            }
-
-            return resultStrs;
-        }
-
-
-        @Override
-        protected String[] doInBackground(String... params) {
-
-            if(params.length==0){
-                return null;
-            }
-
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            String forecastJsonStr=null;
-
-            String mode="json";
-            String units="metric";
-            int numDays=7;
-
-            try {
-
-                final String FORECAST_BASE_URL=
-                        "http://api.openweathermap.org/data/2.5/forecast/daily";
-                final String QUERY_PARAM= "zip";
-                final String FORMAT_PARAM="mode";
-                final String UNITS_PARAM="units";
-                final String DAYS_PARAM="cnt";
-                final String APPID_PARAM="APPID";
-
-                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM,params[0])
-                        .appendQueryParameter(FORMAT_PARAM,mode)
-                        .appendQueryParameter(UNITS_PARAM,units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(APPID_PARAM,BuildConfig.OPEN_WEATHER_MAP_API_KEY)
-                        .build();  //API is stored in gradle.properties and defined in build.gradle
-
-                URL url = new URL(builtUri.toString());
-
-                urlConnection=(HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-
-                if(inputStream==null){
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line=reader.readLine())!=null){
-                    buffer.append(line+"\n");
-                }
-
-                if(buffer.length()==0){
-                    return null;
-                }
-                forecastJsonStr=buffer.toString();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error",e);
-                return null;
-            }finally {
-                if(urlConnection!=null){
-                    urlConnection.disconnect();
-                }
-                if(reader!=null){
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "error closing stream",e);
-                    }
-
-                }
-            }
-
-
-//            Log.v("RETURNED STRING",forecastJsonStr);
-            try {
-                return getWeatherDataFromJson(forecastJsonStr, numDays);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            if(result != null){
-                mforecastAdapter.clear();
-                mforecastAdapter.addAll(result);
-
-            }
-
-
-
-        }
-
-
     }
 
 
