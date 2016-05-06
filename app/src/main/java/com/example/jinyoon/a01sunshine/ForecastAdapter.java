162dbf34548f67d05d540a98093e2b19202bae64
Jin Yoon <jinyoon0124@gmail.com>
2016-05-06 01:04:55 -0400

    Lesson5 UI viewHolder implemented

--- app/src/main/java/com/example/jinyoon/a01sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/jinyoon/a01sunshine/ForecastAdapter.java
@@ -13,6 +13,11 @@ import android.widget.TextView;
  * {@link ForecastAdapter} exposes a list of weather forecasts
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
+
+/**
+ * Cache of the children views for a forecast list item.
+ */
+
 public class ForecastAdapter extends CursorAdapter {
 
     private final int VIEW_TYPE_TODAY=0;
@@ -51,6 +56,22 @@ public class ForecastAdapter extends CursorAdapter {
 //                " - " + highAndLow;
 //    }
 
+    public static class ViewHolder {
+        public final ImageView iconView;
+        public final TextView dateView;
+        public final TextView descriptionView;
+        public final TextView highTempView;
+        public final TextView lowTempView;
+
+        public ViewHolder(View view) {
+            iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+            dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+            descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+            highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
+            lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
+    }
+
     @Override
     public int getItemViewType(int position) {
         return (position==0)? VIEW_TYPE_TODAY:VIEW_TYPE_FUTURE_DAY;
@@ -72,7 +93,11 @@ public class ForecastAdapter extends CursorAdapter {
         }else if(viewType==VIEW_TYPE_FUTURE_DAY){
             layoutId=R.layout.list_item_forecast;
         }
-        return LayoutInflater.from(context).inflate(layoutId, parent, false);
+        View view= LayoutInflater.from(context).inflate(layoutId, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+
+        return view;
     }
 
     @Override
@@ -80,30 +105,28 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+
         // Read weather icon ID from cursor
         int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
         // Use placeholder image for now
-        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
-        iconView.setImageResource(R.mipmap.ic_launcher);
+        viewHolder.iconView.setImageResource(R.mipmap.ic_launcher);
 
         long date = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
-        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
-        dateView.setText(Utility.getFriendlyDayString(mContext,date));
+        viewHolder.dateView.setText(Utility.getFriendlyDayString(mContext,date));
 
         String desc = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
-        TextView descView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
-        descView.setText(desc);
+        viewHolder.descriptionView.setText(desc);
+
         // Read user preference for metric or imperial temperature units
         boolean isMetric = Utility.isMetric(context);
 
         // Read high temperature from cursor
         double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
-        TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
-        highView.setText(Utility.formatTemperature(high, isMetric));
+        viewHolder.highTempView.setText(Utility.formatTemperature(high, isMetric));
 
         double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
-        TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
-        lowView.setText(Utility.formatTemperature(low, isMetric));
-
+        viewHolder.lowTempView.setText(Utility.formatTemperature(low, isMetric));
     }
 }
